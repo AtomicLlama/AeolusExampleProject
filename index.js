8638f39239d925798f8e6b0555848cee048d2333
Mathias Quintero <mathias.quintero@tum.de>
2016-03-17 03:23:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,4 @@ var Aeolus = require('Aeolus');
 
 var port = process.env.PORT || 8080;
 
-Aeolus.auth(function(user,pass) {
-  return user === "root" && pass === "alpine";
-});
-
 Aeolus.createServer(port);
